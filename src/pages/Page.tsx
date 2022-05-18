314b68cac02d8164d4d19d62e0a3dab0eace7d96
Sean Wong <seanwong24@live.com>
2022-05-17 22:07:47 -0700

    prepare for PWA

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,32 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Page.css";
 
 const Page: React.FC = () => {
+  const { name } = useParams<{ name: string }>();
 
-  const { name } = useParams<{ name: string; }>();
+  useEffect(() => {
+    if ("launchQueue" in window && "LaunchParams" in window) {
+      (window as any).launchQueue.setConsumer(
+        (launchParams: { files: any[] }) => {
+          for (const fileHandle of launchParams.files) {
+            console.log(fileHandle);
+            alert("app launched by file");
+          }
+        }
+      );
+    }
+  }, []);
 
   return (
     <IonPage>
