09b4934f834b1aaa9599b58b2609c8fc2f0b9004
Sean Wong <seanwong24@live.com>
2022-05-24 22:13:07 +0000

    add openNotebookFile()

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,12 +1,15 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { open } from "ionicons/icons";
 import { useEffect } from "react";
 import { useParams } from "react-router";
 import ExploreContainer from "../components/ExploreContainer";
@@ -20,8 +23,7 @@ const Page: React.FC = () => {
       (window as any).launchQueue.setConsumer(
         (launchParams: { files: any[] }) => {
           for (const fileHandle of launchParams.files) {
-            console.log(fileHandle);
-            alert("app launched by file");
+            openNotebookFile(fileHandle);
           }
         }
       );
@@ -36,6 +38,11 @@ const Page: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>{name}</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={() => openNotebookFile()}>
+              <IonIcon slot="icon-only" icon={open}></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
 
@@ -49,6 +56,19 @@ const Page: React.FC = () => {
       </IonContent>
     </IonPage>
   );
+
+  async function openNotebookFile(fileHandle?: any) {
+    if (!fileHandle) {
+      [fileHandle] = await (window as any).showOpenFilePicker();
+      if (!fileHandle) {
+        return;
+      }
+    }
+    const file = await fileHandle.getFile();
+    const contents = await file.text();
+    alert('file opened');
+    alert(contents);
+  }
 };
 
 export default Page;
